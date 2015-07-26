1af381d323bfe0d55d7d3edd2be0bd81c76188f4
Oana Rusu <oana.rusu.qa@gmail.com>
2015-07-26 19:44:02 +0100

    working tests

--- marksandspencer/src/test/java/com/marksandspencer/app/BasketSteps.java
+++ marksandspencer/src/test/java/com/marksandspencer/app/BasketSteps.java
@@ -13,8 +13,6 @@ import utility.*;
 
 public class BasketSteps {
 
-	private WebDriver driver = Setup.driver;
-
 	public void setUpWebDriver() throws Exception {
 		Setup.setUp();
 	    Setup.driver.get(Config.URL);
@@ -24,27 +22,29 @@ public class BasketSteps {
 		Setup.tearDown();
 	}
 
-	
-	@Given("^I have added a shirt to my bag$")
-	public void I_have_added_a_shirt_to_my_bag() throws Throwable {
-	   setUpWebDriver();
-	   SearchPage.searchFor(driver, "summer shirt");
-	   IndividualItemPage.addItemToBag(driver);
+	@Given("^I have added \"([^\"]*)\" item to my bag$")
+	public void I_have_added_item_to_my_bag(String itemToSearchFor) throws Throwable {
+		 setUpWebDriver();
+		   SearchPage.searchFor(Setup.driver, itemToSearchFor);
+		   IndividualItemPage.addItemToBag(Setup.driver);
 	}
 
 	@When("^I view the contents of my bag$")
 	public void I_view_the_contents_of_my_bag()  {
-	    IndividualItemPage.viewBagButton(driver).click();
+	    IndividualItemPage.viewBagButton(Setup.driver).click();
 	}
 
-	@Then("^I can see the contents of the bag include a shirt$")
-	public void I_can_see_the_contents_of_the_bag_include_a_shirt() {
-		List<WebElement> listOfItems = YourBagPage.basketList(driver);
-		for (int i = 0; i < listOfItems.size(); i++)
-	    {
-	        WebElement item = listOfItems.get(i);
-	        Assert.assertTrue(item.getText().contains(IndividualItemPage.itemName(driver).getText())); 
-	     }
-	}
+	  @Then("^I can see the contents of the bag include \"([^\"]*)\"$")
+	  public void I_can_see_the_contents_of_the_bag_include(String itemName) throws Throwable {
+		  List<WebElement> listOfItems = YourBagPage.basketList(Setup.driver);
+		  for (int i = 0; i < listOfItems.size(); i++)
+	      {
+	          WebElement item = listOfItems.get(i);
+	          Assert.assertTrue(item.getText().contains(itemName));
+	      }
+		  tidyUp();
+	  }
+
+
 
 }
